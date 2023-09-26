89ceded4d0dc2b8a2d9a8056077fa03832408f68
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-09-26 13:16:42 -0300

    calculo suma

---
+++ MiSuma.java
@@ -0,0 +1,11 @@
+//Suma de dos números: Se le solicita que guarden dos números enteros y los
+//sumen. El resultado mostrarlo por pantalla.
+
+public class MiSuma {
+    public static void main(String args[]){
+        var numero1 = 15;
+        var numero2 = 20;
+        var resultado = numero1 + numero2;
+        System.out.println("la suma entre " + numero1 + " y " +  numero2 + " es: " + resultado);
+    }
+}
