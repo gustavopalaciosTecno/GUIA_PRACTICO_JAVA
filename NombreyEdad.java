6bf4771389e68e5a2348ca2c1c4adbf36cb951d3
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-09-26 13:24:31 -0300

    ejercicio nombre y edad

---
+++ NombreyEdad.java
@@ -0,0 +1,12 @@
+//8. Mi nombre y edad: Se le solicita que guarde en un espacio de memoria su nombre
+//y apellido, luego guarde su edad e imprimalos por pantalla.
+public class NombreyEdad {
+    public static void main(String args[]){
+
+        var nombre = "Néstor Gustavo";
+        var edad = 42;
+        System.out.println("mi nombre es: " + nombre);
+        System.out.println("Mi edad es: " + edad);
+
+    }
+}
