cf9d1674775192b21c77fe573233430dbce15371
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-09-30 18:27:27 -0300

    contador negativo

---
+++ MiDescontador.java
@@ -0,0 +1,12 @@
+//Descontador: Se le solicita que guarde un número en un espacio de memoria y
+//discretamente dicho número 20 veces sin usar asignación. ¿Que operador podría
+//usar para hacerlo?
+public class MiDescontador {
+    public static void main(String args[]){
+        int numero = 1;
+        for (int i=0 ;i<21;++i){
+            numero--;
+            System.out.println(numero);
+    }
+}
+}
