ac24425436380f5e067d9d718f5ad6fad3cec9eb
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-09-30 21:22:24 -0300

    contador

--- MiContador.java
+++ MiContador.java
@@ -1,3 +1,4 @@
+// Ejercicios nivel : Padawan JAVA JEDI
 //Contador: Se le solicita que guarde un número en un espacio de memoria e
 //incremente dicho número 20 veces sin usar asignación. ¿Que operador podría usar
 //para hacerlo?
