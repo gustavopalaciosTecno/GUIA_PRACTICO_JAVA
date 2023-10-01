bf17e5e9050c8ce90cb6169c90f478df5fd57022
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-09-30 21:21:14 -0300

    convertir de libra a kilogramos

---
+++ ConversionAKilogramos.java
@@ -0,0 +1,18 @@
+//5. Conversión a kilogramos: Guarde en un espacio de memoria sus libras y luego, en
+//otro espacio de memoria realice la conversión de esas libras a kilogramos. Utilice
+//una constante de los valores que no van a cambiar en esta conversión. Muestre el
+//resultado por pantalla
+
+import java.util.Scanner;
+
+public class ConversionAKilogramos {
+    public static void main(String args[]){
+        Scanner consola = new Scanner(System.in);
+        System.out.println("Coloca un valor en libras: ");
+        double constanteLibra = 0.453592;
+        double libra = consola.nextDouble();
+        double kilo=libra * constanteLibra;
+        System.out.println("El valor de "+libra+" Libras son " +kilo+ " Kg");
+        consola.close();
+    }
+}
