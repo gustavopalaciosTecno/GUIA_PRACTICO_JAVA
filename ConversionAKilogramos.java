ffece74bde61e4325b5ee2c18b0afe64585e7faa
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-09-30 21:25:03 -0300

    Conversión de libras a kilogramos

--- ConversionAKilogramos.java
+++ ConversionAKilogramos.java
@@ -1,3 +1,4 @@
+// Ejercicios nivel : Padawan JAVA JEDI
 //5. Conversión a kilogramos: Guarde en un espacio de memoria sus libras y luego, en
 //otro espacio de memoria realice la conversión de esas libras a kilogramos. Utilice
 //una constante de los valores que no van a cambiar en esta conversión. Muestre el
