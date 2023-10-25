34af47ba046797e880fa38a3a64b685e23aa064e
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-10-25 09:57:42 -0300

    arreglos

---
+++ MiArray.java
@@ -0,0 +1,7 @@
+public class MiArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        System.out.println("el valor del array es: " + arr.length);
+        System.out.println("el valor de la posición 4 es: " + arr[2]);
+    }
+}
